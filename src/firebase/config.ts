b764bc7e83d46f2549c47949f7f1e1a454fbca00
Finisherop <160778489+Finisherop@users.noreply.github.com>
2025-10-06 20:42:53 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: "AIzaSyD-EiTLr-bDDDKgR5tvzguyNfdlKDO8Rw8",
   authDomain: "tap-and-earn-d3583.firebaseapp.com",
+  databaseURL: "https://tap-and-earn-d3583-default-rtdb.firebaseio.com",
   projectId: "tap-and-earn-d3583",
   storageBucket: "tap-and-earn-d3583.firebasestorage.app",
   messagingSenderId: "759083332180",
   appId: "1:759083332180:web:165eb0bf070956fb0033c1"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
